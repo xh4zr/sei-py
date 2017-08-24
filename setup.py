358cb62cdcf46a8246bdf92d381635bc86b49b10
Evan Anderson <evan.anderson@caveon.com>
2017-08-24 16:37:24 -0600

    set up for pypi upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name = 'sei_py',
+    packages = ['sei_py'],
+    version = '0.0.1',
+    description = 'A Caveon SEI helper library',
+    author = 'Evan Anderson',
+    author_email = 'evan.anderson@caveon.com',
+    url = 'https://github.com/xh4zr/sei_py',
+    download_url = 'https://github.com/xh4zr/sei_py/archive/0.1.tar.gz',
+    keywords = ['caveon', 'sei', 'exam', 'test security'],
+    classifiers = []
+)
