86adf7e04ed97fe7de6a158ce6cfc61b9dfc1c97
Evan Anderson <evan.anderson@caveon.com>
2017-10-11 15:59:26 -0600

    update setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name = 'sei_py',
     packages = ['sei_py', 'sei_py.base', 'sei_py.rest'],
-    version = '0.1.9',
+    version = '0.2.0',
     description = 'A Caveon SEI helper library',
     author = 'Evan Anderson',
     author_email = 'evan.anderson@caveon.com',
     url = 'https://github.com/xh4zr/sei-py',
     keywords = ['caveon', 'sei', 'exam', 'test security'],
-    install_requires = ['requests==2.18.4']
+    install_requires = ['requests==2.18.4', 'python_jwt==2.0.2']
 )
